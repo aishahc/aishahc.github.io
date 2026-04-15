b1d3f3253c677c54c860184172c528f51c1385ff
aishahc <57232038+aishahc@users.noreply.github.com>
2026-04-15 13:06:21 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -18,8 +18,8 @@ $.getJSON(weatherAlertsUrl, function(data) {
     style: function(feature){
       var alertColor = 'orange';
       if (feature.properties.severity === 'Severe') alertColor = 'red';
-      if (feature.properties.severity === 'Extreme') alertColor = 'black';
-      if (feature.properties.severity === 'Minor') alertColor = 'blue';
+      else if (feature.properties.severity === 'Extreme') alertColor = 'black';
+      else if (feature.properties.severity === 'Minor') alertColor = 'blue';
       return { color: alertColor };
     },
     
